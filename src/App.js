e0600e37764fd45f6e60d42710cd19fbc12ca7dd
adeesha.nichelabs <adeesha.mahalekam@nichelabs.lk>
2022-05-01 18:47:14 +0530

    add basic ui & routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
+import { Routes, Route } from "react-router-dom";
+import Header from "./Components/Header";
+import LoginPage from "./Pages/LoginPage";
+import InfoPage from "./Pages/InfoPage";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>LibSys</p>
-      </header>
+      <Header />
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="borrowInfo" element={<InfoPage />} />
+        <Route
+          path="*"
+          element={
+            <main style={{ padding: "1rem" }}>
+              <p className="text-danger">404 Page not found</p>
+            </main>
+          }
+        />
+      </Routes>
     </div>
   );
 }
